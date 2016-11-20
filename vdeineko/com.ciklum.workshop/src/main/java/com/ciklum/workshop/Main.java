5b12cd34c6c542c37922e0414da4b0e426079fcd
vdeinenko <viktoria.deinenko@ricardo.ch>
2016-11-21 00:34:36 +0200

    Main java file

---
+++ vdeineko/com.ciklum.workshop/src/main/java/com/ciklum/workshop/Main.java
@@ -0,0 +1,10 @@
+package com.ciklum.workshop;
+
+/**
+ * Created by vdeineko on 11/21/16.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello from Viktoria Deineko");
+    }
+}
